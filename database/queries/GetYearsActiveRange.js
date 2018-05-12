046bea1aaf0b7cf3f431bfbc419aa7264cadbd70
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-05-12 13:33:58 -0700

    Wrote the search artists query.

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -7,9 +7,9 @@ const Artist = require('../models/artist');
  */
 module.exports = () => {
   const minQuery = Artist.find({}).sort({ yearsActive: 1 }).limit(1)
-    .then(artists => artists[0].age);
+    .then(artists => artists[0].yearsActive);
   const maxQuery = Artist.find({}).sort({ yearsActive: -1 }).limit(1)
-    .then(artists => artists[0].age);
+    .then(artists => artists[0].yearsActive);
 
   return Promise.all([minQuery, maxQuery])
     .then(values => {
